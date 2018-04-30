073aef98f364ccc75083b410eb1c1ce863f44b2d
M Farkas-Dyck <strake888@gmail.com>
2018-04-30 01:19:48 -0800

    refactorize YFind

--- src/YFind.hs
+++ src/YFind.hs
@@ -2,11 +2,12 @@
 
 module YFind (Parms (..), go) where
 
-import Prelude hiding (last, replicate)
+import Prelude hiding (head, last, replicate)
 import Control.Monad
 import Control.Monad.Trans.Maybe
 import Data.Bool
 import Data.Foldable
+import Data.List.NonEmpty (NonEmpty (..), head, last, (<|))
 import Data.Rule.Hex
 import Numeric.Natural
 import System.IO.Unsafe
@@ -19,26 +20,32 @@ data Parms = Parms { speed :: ((Int, Word), Word), size :: (Word, Word) }
   deriving (Eq, Read, Show)
 
 go :: Rule -> Parms -> Maybe [Natural]
-go rule (Parms { speed = ((dx, fi -> dy), fi -> period)
-                 , size = (fi -> width, fi -> height) }) = unsafePerformIO . evalZ3 $ do
+go rule parms = unsafePerformIO . evalZ3 $ setup rule parms >>= runMaybeT . getRows . head
+
+setup :: MonadZ3 z3 => Rule -> Parms -> z3 (NonEmpty [AST])
+setup rule (Parms { speed = ((dx, fi -> dy), fi -> period)
+                 , size = (fi -> width, fi -> height) }) = do
     zeroRow <- mkBitvector (fi width) 0
-    rows <- replicateA height $ mkFreshBvVar "row" (fi width)
-    rows' <- (iterateM period $ evolveRows rule) rows
+    rowses <- replicateA height (mkFreshBvVar "row" (fi width)) >>= iterateM period (evolveRows rule)
+    let rows  = head rowses
+        rows' = last rowses
 
     (bind2 . zipWithM_) (curry $ assert <=< uncurry mkEq)
         (traverse (pad dx)          (rows ++ replicate dy zeroRow))
         (traverse (pad (negate dx)) (replicate dy zeroRow ++ rows'))
-    assert =<< mkNot =<< mkEq zeroRow =<< foldrM mkBvor zeroRow rows
-    runMaybeT $ do
-        model <- MaybeT (snd <$> solverCheckAndGetModel)
-        fmap fi <$> MaybeT (mapEval evalInt model rows)
+    rowses <$ (assert =<< mkNot =<< mkEq zeroRow =<< foldrM mkBvor zeroRow rows)
+
+getRows :: MonadZ3 z3 => [AST] -> MaybeT z3 [Natural]
+getRows rows = do
+    model <- MaybeT (snd <$> solverCheckAndGetModel)
+    fmap fi <$> MaybeT (mapEval evalInt model rows)
 
 pad :: MonadZ3 z3 => Int -> AST -> z3 AST
 pad 0 row = pure row
 pad n row = bool flip id (n < 0) mkConcat row =<< mkBitvector (abs n) 0
 
-iterateM :: Monad m => Natural -> (a -> m a) -> a -> m a
-iterateM 0 _ = pure
-iterateM k f = f >=> iterateM (k-1) f
+iterateM :: Monad m => Natural -> (a -> m a) -> a -> m (NonEmpty a)
+iterateM 0 _ x = pure (x:|[])
+iterateM k f x = (x <|) <$> (f x >>= iterateM (k-1) f)
 
 fi = fromIntegral
