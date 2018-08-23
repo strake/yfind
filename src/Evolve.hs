d6446df9dd45f25ca29e7191e98a1b5cf0a314b7
M Farkas-Dyck <strake888@gmail.com>
2018-08-22 21:15:59 -0800

    refactorize `evolve`

--- src/Evolve.hs
+++ src/Evolve.hs
@@ -3,10 +3,13 @@
 module Evolve (evolve) where
 
 import Prelude hiding (replicate)
+import Control.Applicative
 import Control.Monad
 import Data.Array
 import Data.Bool
 import Data.Foldable
+import Data.Functor.Identity
+import Data.Functor.Product
 import Data.Maybe (fromMaybe)
 import Data.Traversable
 import Util
@@ -16,15 +19,22 @@ import Z3.Tagged
 import Rule
 
 evolve :: (Num i, Ix i, Applicative f, Traversable f) => Rule (i, i) f Bool -> Array (i, i) (AST s) -> Z3 s (Array (i, i) (AST s))
-evolve (Rule {..}) = \ a -> do
+evolve (Rule {..}) = evolve' nbhd $ \ theActualNbhd ->
+    [cell
+       | cell <- mkFreshConst "cell" =<< mkBoolSort
+       , () <- assert <=< mkOr <=< for (sequenceA $ pure [False, True]) $ \ theNbhd ->
+               mkAnd . toList =<< traverse2 (bool mkNot pure) (Pair <*> Identity . evolveCell $ theNbhd)
+                                                              (Pair theActualNbhd $ Identity cell)]
+
+evolve' :: (Num i, Ix i, Applicative f)
+        => ((i, i) -> f (i, i)) -> (f (AST s) -> Z3 s (AST s)) -> Array (i, i) (AST s) -> Z3 s (Array (i, i) (AST s))
+evolve' nbhd evolveCell = \ a -> do
     false <- mkBool False
-    true  <- mkBool True
-    let evolveNbhd ix = mkOr <=< for (sequenceA $ pure [False, True]) $ \ theNbhd ->
-                        bool (pure false) `flip` evolveCell theNbhd $
-                        mkAnd . toList =<<
-                        traverse2 mkEq (bool false true <$> theNbhd)
-                                       (fromMaybe false . (a !?) <$> nbhd ix)
-    x <- traverse (getSort >=> mkFreshConst "cell") a
-    x <$ (range . expandedBounds) a `for` \ ix -> assert =<< mkEq (fromMaybe false $ x !? ix) =<< evolveNbhd ix
+    let evolveNbhd = evolveCell . fmap (fromMaybe false . (a !?)) . nbhd
+    -- boundary conditions
+    for_ (range $ expandedBounds a) $ \ ix -> case a !? ix of
+        Nothing -> assert =<< mkEq false =<< evolveNbhd ix
+        Just _ -> pure ()
+    liftA2 fmap array (traverse (liftA2 fmap (,) evolveNbhd) . range) (bounds a)
   where expandedBounds a = let ((il, jl), (ih, jh)) = bounds a
                            in ((il-1, jl-1), (ih+1, jh+1))
